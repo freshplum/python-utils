9f575667c4ecdc56b47f33d13c1930b373939359
Sam Odio <sam@odio.com>
2012-04-04 11:32:09 -0700

    math.floor and math.ceil for datetimes

--- date.py
+++ date.py
@@ -24,3 +24,33 @@ def serialize_timestamp(dt_object, format=LONG_TIMESTAMP_FORMAT, short=False):
     if short:
         format = SHORT_TIMESTAMP_FORMAT
     return datetime.datetime.strftime(dt_object, format)
+
+G = {
+        'SECOND': 6,
+        'MINUTE': 5,
+        'HOUR'  : 4,
+        'DAY'   : 3
+    }
+
+def floor(dt, granularity):
+    """
+    Floors (similar to math.floor) the date to nearest day, hour, minute, or second
+    """
+    g = G.get(granularity.upper(), 6)
+    return datetime.datetime(*dt.timetuple()[0:g])
+
+
+def ceil(dt, granularity):
+    """
+    Similar to math.ceil.  Rounds time up to nearest day, hour, minute, or second.
+    """
+    g = G.get(granularity.upper(), 6)
+    d = list(dt.timetuple()[:g])
+
+    #Check if any of the less significant times exist and are not 0
+    if any(dt.timetuple()[-g:]):
+        #Round up
+        d[g-1]+=1
+        
+    return datetime.datetime(*d)
+    
